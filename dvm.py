fb4f657850be25848b9679848ac10f4ffa0f5921
Guy John <guy@rumblesan.com>
2012-07-29 22:38:24 +0100

    Adding beginings of Diddy VM
    Currently Diddy VM class has some helper functions for setting and
    getting internal memory values
    
    These all have tests

---
+++ dvm.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+# This is the class that contains all the logic for the Diddy VM
+# The VM has two sections, the RAM and the Stack
+# I probablly only need one or the other of these
+# but right now I don't know what I'm doing so just roll with it
+
+class DVM(object):
+
+    def __init__(self):
+        self.ram = dict.fromkeys(xrange(2 ** 12), 0)
+        self.stack = []
+        self.position = 0
+
+    # Language Instructions
+    def move(self, A, B):
+        pass
+
+    def compare(self, A, B):
+        pass
+
+    def branch(self, A, B):
+        pass
+
+    def add(self, A, B):
+        pass
+
+    def subtract(self, A, B):
+        pass
+
+    # General purpose functions
+    def executeNextInstruction(self):
+        pass
+
+    def getValue(self, addr):
+        """
+        Retrieves a value based on the input
+        Address values are always 4 characters and correspond to RAM
+        addresses, literal values or the top value popped off the stack
+
+        Addresses in RAM are prefixed by a *
+            e.g.  *00F corresponds to the 16th position in RAM
+
+        Literal values are prefixed with a #
+            e.g.  #F50 is a value of 3920
+
+        Stack values are prefixed with an 's', the remaining
+        three chars don't matter. The value returned will be
+        the next value popped off the stack
+        """
+        t = addr[0]
+        pos = addr[1:]
+        if t == '*':
+            memAddr = self.hex2int(pos)
+            output = self.getMem(memAddr)
+        elif t == '#':
+            output = self.hex2int(pos)
+        elif t == 's':
+            output = self.pop()
+        return output
+
+    def setValue(self, addr, val):
+        """
+        Sets the value in the correct structure. Either stack or RAM
+        Address values are always 4 characters and correspond to RAM
+        addresses or a command to push the value to the stack
+
+        Addresses in RAM are prefixed by a *
+            e.g.  *00F corresponds to the 16th position in RAM
+
+        Stack values are prefixed with an 's', the remaining
+        three chars don't matter. The value is pushed onto the stack
+        """
+        t = addr[0]
+        pos = addr[1:]
+        if t == '*':
+            memAddr = self.hex2int(pos)
+            self.setMem(memAddr, val)
+        elif t == '#':
+            pass
+        elif t == 's':
+            self.push(val)
+
+    def setMem(self, addr, val):
+        self.ram[addr] = val
+
+    def getMem(self, addr):
+        return self.ram[addr]
+
+    def push(self, v):
+        self.stack.append(v)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def hex2int(self, val):
+        return int(val, 16)
+
+    def int2hex(self, val):
+        return hex(val)[2:].rjust(3, '0')
+
+
+
+if __name__ == '__main__':
+    import unittest
+    from tests.testdvm import TestDVM
+    unittest.main()
+
---
+++ tests/testdvm.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import unittest
+from dvm import DVM
+
+class TestDVM(unittest.TestCase):
+
+    def setUp(self):
+        self.dvm = DVM()
+
+    def tearDown(self):
+        del self.dvm
+
+    def test_creation(self):
+        self.assertIsInstance(self.dvm, DVM)
+
+    def testPushPop(self):
+        self.dvm.push(1)
+        self.dvm.push(2)
+        self.dvm.push(3)
+        self.assertEqual(self.dvm.pop(), 3)
+        self.assertEqual(self.dvm.pop(), 2)
+        self.assertEqual(self.dvm.pop(), 1)
+
+    def testSetGetMem(self):
+        self.dvm.setMem(1000, 1)
+        self.dvm.setMem(2000, 2)
+        self.dvm.setMem(3000, 3)
+        self.assertEqual(self.dvm.getMem(1000), 1)
+        self.assertEqual(self.dvm.getMem(2000), 2)
+        self.assertEqual(self.dvm.getMem(3000), 3)
+
+    def testHexToInt(self):
+        self.assertEqual(self.dvm.hex2int('fff'), 4095)
+        self.assertEqual(self.dvm.hex2int('0ff'), 255)
+        self.assertEqual(self.dvm.hex2int('000'), 0)
+
+    def testIntToHex(self):
+        self.assertEqual(self.dvm.int2hex(4095), 'fff')
+        self.assertEqual(self.dvm.int2hex(255), '0ff')
+        self.assertEqual(self.dvm.int2hex(0), '000')
+
+    def testSetAddrValue(self):
+        self.dvm.setValue('*000', 123)
+        self.dvm.setValue('*fff', 111)
+        self.dvm.setValue('*b8a', 2954)
+        self.assertEqual(self.dvm.getMem(0), 123)
+        self.assertEqual(self.dvm.getMem(4095), 111)
+        self.assertEqual(self.dvm.getMem(2954), 2954)
+
+    def testGetAddrValue(self):
+        self.dvm.setMem(0, 1)
+        self.dvm.setMem(4095, 1111)
+        self.dvm.setMem(3258, 555)
+        self.assertEqual(self.dvm.getValue('*000'), 1)
+        self.assertEqual(self.dvm.getValue('*fff'), 1111)
+        self.assertEqual(self.dvm.getValue('*cba'), 555)
+
+    def testSetStackValue(self):
+        # Only first letter of address matters
+        # Needs to be an 's' for stack operations
+        self.dvm.setValue('stck', 123)
+        self.dvm.setValue('stck', 111)
+        self.dvm.setValue('stck', 2954)
+        self.assertEqual(self.dvm.pop(), 2954)
+        self.assertEqual(self.dvm.pop(), 111)
+        self.assertEqual(self.dvm.pop(), 123)
+
+    def testGetStackValue(self):
+        # Only first letter of address matters
+        # Needs to be an 's' for stack operations
+        self.dvm.push(123)
+        self.dvm.push(111)
+        self.dvm.push(2954)
+        self.assertEqual(self.dvm.getValue('stck'), 2954)
+        self.assertEqual(self.dvm.getValue('stck'), 111)
+        self.assertEqual(self.dvm.getValue('stck'), 123)
+
+    def testGetLiteralValue(self):
+        self.assertEqual(self.dvm.getValue('#fff'), 4095)
+        self.assertEqual(self.dvm.getValue('#000'), 0)
+        self.assertEqual(self.dvm.getValue('#cba'), 3258)
+
+    def testGetSetValue(self):
+        self.dvm.setValue('stck', 123)
+        self.dvm.setValue('stck', 111)
+        self.dvm.setValue('*bbb', 3003)
+        self.dvm.setValue('*fff', 4095)
+        self.assertEqual(self.dvm.getValue('stck'), 111)
+        self.assertEqual(self.dvm.getValue('*bbb'), 3003)
+        self.assertEqual(self.dvm.getValue('#aaa'), 2730)
+        self.assertEqual(self.dvm.getValue('*fff'), 4095)
+        self.assertEqual(self.dvm.getValue('stck'), 123)
+
+
+
+
