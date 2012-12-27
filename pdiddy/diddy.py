132435bc9176f465b444a1a4815b2f1f138ed82d
Guy John <guy@rumblesan.com>
2012-12-27 20:04:49 +0000

    Adding debugging console
    Gives some basic commands
        Step through program one instruction at a time
        check memory locations
        view stack

--- pdiddy/diddy.py
+++ pdiddy/diddy.py
@@ -6,13 +6,37 @@ from argparse import ArgumentParser
 
 from struct import unpack
 
+import re
+
+consoleParse = re.compile('(?P<command>[a-z]+) *(?P<arg>[0-9]+)?')
+
 def parseArgs():
     parser = ArgumentParser(description='DiddyVM')
     parser.add_argument('program', help='the program to run')
     parser.add_argument('-d', '--debug', help='enable debugging', action='store_true')
     parser.add_argument('-s', '--stack', help='enable stack display', action='store_true')
+    parser.add_argument('-c', '--console', help='enable debugging console', action='store_true')
 
     return parser.parse_args()
+
+def debugging_console(dvm):
+
+    while dvm.running == True:
+        user_input = raw_input("# ")
+        result = consoleParse.search(user_input)
+
+        if result:
+            c = result.group('command')
+            if c == 'mem' and result.group('arg'):
+                v = int(result.group('arg'))
+                print(dvm.getMem(v))
+            elif c == 'stack':
+                print(dvm.stack)
+            elif c == 'next':
+                dvm.executeNextInstruction()
+        else:
+            dvm.executeNextInstruction()
+
 
 def main():
 
@@ -33,8 +57,11 @@ def main():
 
     dvm.loadProgram(programData)
 
-    while dvm.running == True:
-        dvm.executeNextInstruction()
+    if args.console:
+        debugging_console(dvm)
+    else:
+        while dvm.running == True:
+            dvm.executeNextInstruction()
 
 if __name__ == '__main__':
     main()
