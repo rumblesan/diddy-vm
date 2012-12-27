31ff4b22c08092025e1e7442481216e6b8b6c2d3
Guy John <guy@rumblesan.com>
2012-12-27 15:29:40 +0000

    Updating the assembler with new functionality

--- tools/assembler.py
+++ tools/assembler.py
@@ -6,7 +6,8 @@ from os.path import basename, splitext, join
 
 from struct import pack
 
-lineParse = re.compile('^((?P<label>[a-zA-Z0-9]*): *| *)(?P<data>[a-zA-Z0-9]*)')
+labelParse = re.compile('^(?P<label>[a-z0-9]+):')
+lineParse = re.compile('^(?:[a-z0-9]*:)? *(((?P<pflag>\*)?(?P<dflag>#)?(?P<inst>[A-Z]+)( *(?P<pointer>[a-z0-9]+))?)|(?P<data>[a-zA-Z0-9]+))')
 
 
 def parseArgs():
@@ -21,9 +22,10 @@ def calculateLabels(programData):
     labels = {}
     num = 1
     for line in programData:
-        label = lineParse.search(line).group('label')
+        label = labelParse.search(line)
         if label:
-            labels[label] = num
+            l = label.group('label')
+            labels[l] = num
         num += 1
 
     return labels
@@ -35,16 +37,29 @@ def assembleProgram(programData, labels, instructions):
 
     for line in programData:
 
-        data = lineParse.search(line).group('data')
+        data = lineParse.search(line)
 
         if not line:
             output.append(0)
-        elif data in labels:
-            output.append(labels[data])
-        elif data in instructions:
-            output.append(instructions[data])
-        else:
-            output.append(int(data))
+        elif data.group('data'):
+            d = data.group('data')
+            if d in labels:
+                output.append(labels[d])
+            else:
+                output.append(int(d))
+        elif data.group('inst'):
+            v = instructions[data.group('inst')]
+            if data.group('dflag'):
+                v = v | (1 << 27)
+            if data.group('pflag'):
+                v = v | (1 << 26)
+            if data.group('pointer'):
+                p = data.group('pointer')
+                if p in labels:
+                    v = v | labels[p]
+                else:
+                    v = v | int(p)
+            output.append(v)
 
     return output
 
@@ -52,17 +67,18 @@ def setupInstructions():
 
     i = {}
 
-    i['NOP'] =      0
-    i['COPY'] =     1
-    i['JUMP'] =     2
-    i['BRANCH'] =   3
-    i['EQUAL'] =    4
-    i['GREATER'] =  5
-    i['LESSER'] =   6
-    i['ADD'] =      7
-    i['SUBTRACT'] = 8
-    i['OUTPUT'] =   9
-    i['HALT'] =     10
+    i['NOP'] =      0 << 28
+    i['PUSH'] =     1 << 28
+    i['POP'] =      2 << 28
+    i['JUMP'] =     3 << 28
+    i['BRANCH'] =   4 << 28
+    i['EQUAL'] =    5 << 28
+    i['GREATER'] =  6 << 28
+    i['LESSER'] =   7 << 28
+    i['ADD'] =      8 << 28
+    i['SUBTRACT'] = 9 << 28
+    i['OUTPUT'] =   10 << 28
+    i['HALT'] =     11 << 28
 
     return i
 
