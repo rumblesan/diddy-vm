b3b8383b8dc049ad78115308572406c0412f2ea8
Guy John <guy@rumblesan.com>
2012-12-29 09:25:35 +0000

    Adding more tests for vmbase functions

--- cdiddy/src/unit_tests.c
+++ cdiddy/src/unit_tests.c
@@ -11,13 +11,136 @@
 int tests_run = 0;
 
 static char * test_creation() {
-    DVM vmbase = setup_vmbase();
-    mu_assert("Error: DVM object not created correctly", vmbase->running == 1);
+    DVM dvm = setup_vmbase();
+
+    mu_assert("Error: DVM object not created correctly", dvm->running == 1);
+
+    cleanup_dvm(dvm);
     return 0;
 }
 
+static char * test_execute_next_instruction() {
+    DVM dvm = setup_vmbase();
+
+    dvm->instructions[1] = nop;
+    dvm->position = 100;
+    dvm->ram[100] = 1 << 28;
+    execute_next_instruction(dvm);
+
+    mu_assert("Error: DVM should be running", dvm->running == 1);
+
+    cleanup_dvm(dvm);
+    return 0;
+}
+
+static char * test_set_instruction_pointer() {
+    DVM dvm = setup_vmbase();
+
+    mu_assert("Error: instruction pointer should be 1", dvm->position == 1);
+    set_instruction_pointer(dvm, 100);
+    mu_assert("Error: instruction pointer should be 100", dvm->position == 100);
+    set_instruction_pointer(dvm, 200);
+    mu_assert("Error: instruction pointer should be 200", dvm->position == 200);
+
+    cleanup_dvm(dvm);
+    return 0;
+}
+
+static char * test_next_instruction_pointer() {
+    DVM dvm = setup_vmbase();
+
+    mu_assert("Error: instruction pointer should be 1", dvm->position == 1);
+    next(dvm);
+    mu_assert("Error: instruction pointer should be 2", dvm->position == 2);
+    next(dvm);
+    mu_assert("Error: instruction pointer should be 3", dvm->position == 3);
+
+    cleanup_dvm(dvm);
+    return 0;
+}
+
+static char * test_stack() {
+    DVM dvm = setup_vmbase();
+
+    mu_assert("Error: Stack poition should be -1", dvm->stack_position == -1);
+    push_stack(dvm, 0);
+    push_stack(dvm, 1);
+    push_stack(dvm, 2);
+    mu_assert("Error: Stack poition should be 3", dvm->stack_position == 2);
+
+    mu_assert("Error: Popped value should be 2", pop_stack(dvm) == 2);
+    mu_assert("Error: Popped value should be 1", pop_stack(dvm) == 1);
+    mu_assert("Error: Popped value should be 0", pop_stack(dvm) == 0);
+
+    cleanup_dvm(dvm);
+    return 0;
+}
+
+static char * test_set_mem() {
+    DVM dvm = setup_vmbase();
+
+    setMem(dvm, 10, 100);
+
+    mu_assert("Error: DVM memory not set correctly", dvm->ram[10] == 100);
+
+    cleanup_dvm(dvm);
+    return 0;
+}
+
+static char * test_get_mem() {
+    DVM dvm = setup_vmbase();
+
+    dvm->ram[10] = 100;
+
+    mu_assert("Error: getMem not returning correct value", getMem(dvm, 10) == 100);
+
+    dvm->position = 20;
+    dvm->ram[20] = 200;
+
+    mu_assert("Error: getMem not returning correct value", getMem(dvm, -1) == 200);
+
+    cleanup_dvm(dvm);
+    return 0;
+}
+
+static char * test_exit() {
+    DVM dvm = setup_vmbase();
+
+    system_exit(dvm, 5);
+    mu_assert("Error: DVM running should be 0", dvm->running == 0);
+    mu_assert("Error: DVM status should be 5", dvm->status == 5);
+
+    cleanup_dvm(dvm);
+    return 0;
+}
+
+static char * test_load_program() {
+    DVM dvm = setup_vmbase();
+
+    uint32_t program_data[4] = {1, 2, 3, 4};
+
+    load_program(dvm, program_data, 4);
+
+    mu_assert("Error: memory value should be 1", dvm->ram[1] == 1);
+    mu_assert("Error: memory value should be 2", dvm->ram[2] == 2);
+    mu_assert("Error: memory value should be 3", dvm->ram[3] == 3);
+    mu_assert("Error: memory value should be 4", dvm->ram[4] == 4);
+
+    cleanup_dvm(dvm);
+    return 0;
+}
+
+
 static char * all_tests() {
     mu_run_test(test_creation);
+    mu_run_test(test_execute_next_instruction);
+    mu_run_test(test_set_instruction_pointer);
+    mu_run_test(test_next_instruction_pointer);
+    mu_run_test(test_stack);
+    mu_run_test(test_get_mem);
+    mu_run_test(test_set_mem);
+    mu_run_test(test_exit);
+    mu_run_test(test_load_program);
     return 0;
 }
 
