ac16cb38228e20ba3ed746203debc6e89d61f942
Guy John <guy@rumblesan.com>
2012-12-30 12:41:23 +0000

    vmbase and dvm tests now run at the same time

--- cdiddy/src/all_tests.c
+++ cdiddy/src/all_tests.c
@@ -3,22 +3,44 @@
 
 #include "min_unit.h"
 #include "test_vmbase.h"
+#include "test_dvm.h"
 
-
-int tests_run = 0;
-
-
+int tests_run;
 
 int main(int argc, char *argv[]) {
-    char *result = all_tests();
-    if (result != 0) {
-        printf("%s\n", result);
-    } else {
-        printf("ALL TESTS PASSED\n");
-    }
-    printf("Tests run: %d\n", tests_run);
 
-    return result != 0;
+    printf("\n");
+    printf("*****************\n");
+    printf("* Running tests *\n");
+    printf("*****************\n\n");
+
+    int result = 0;
+
+    tests_run = 0;
+    char *vmbase_results = test_vmbase();
+    if (vmbase_results != 0) {
+        printf("%s\n", vmbase_results);
+    } else {
+        printf("VMBASE TESTS PASSED\n");
+    }
+    printf("    Tests run: %d\n", tests_run);
+    printf("\n");
+
+    result = result || (vmbase_results != 0);
+
+    tests_run = 0;
+    char *dvm_results = test_dvm();
+    if (dvm_results != 0) {
+        printf("%s\n", dvm_results);
+    } else {
+        printf("DVM TESTS PASSED\n");
+    }
+    printf("    Tests run: %d\n", tests_run);
+    printf("\n");
+
+    result = result || (dvm_results != 0);
+
+    return result;
 }
 
 
