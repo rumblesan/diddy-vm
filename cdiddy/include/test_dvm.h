5c1d37f222d9a38b0d44f90166a271eeeab5f56e
Guy John <guy@rumblesan.com>
2012-12-30 12:32:22 +0000

    Adding DVM test files

---
+++ cdiddy/include/test_dvm.h
@@ -0,0 +1,6 @@
+#ifndef TEST_DVM_H
+#define TEST_DVM_H
+
+char * test_dvm();
+
+#endif
---
+++ cdiddy/src/test_dvm.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "dvm.h"
+#include "vmbase.h"
+#include "min_unit.h"
+
+static char * test_dvm_creation() {
+    DVM dvm = setup_diddy();
+
+    mu_assert("Error: DVM object not created correctly", dvm->running == 1);
+
+    cleanup_dvm(dvm);
+    return 0;
+}
+
+
+char * test_dvm() {
+
+    mu_run_test(test_dvm_creation);
+
+    return 0;
+}
+
